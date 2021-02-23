9d00d777d070dc0d9f6562c824c1d6ed3f1263db
chinsulchinsul <79474779+chinsulchinsul@users.noreply.github.com>
2021-02-23 12:37:43 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
- 
 # This script installs packages automatically.
+printf "\n##################Update##################\n"
+sudo apt update -y
 
-echo #START INSTALLING GIT….
-sudo apt-get update –y
-sudo apt-get install git -y
-cd /opt
-sudo git clone https://github.com/chinsulchinsul/Report_SSH_Log.git
-sudo git checkout .
-sudo chmod 777 Report_SSH_Log #for_learning_purpose
-
-sudo apt-get install openssh -y
+printf "\n##################INSTALLING OPENSSH-SERVER##################\n"
+sudo apt install openssh-server -y
 sudo sed -i -e 's/#PermitRootLogin\swithout-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 sudo service ssh restart
 ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y 
 
-echo ""
-echo "Please Input IP Server : ";
+printf "\nPlease Input IP Server : ";
 read ipserver;
 ssh-copy-id -i ~/.ssh/id_rsa.pub root@$ipserver 
 
-echo ""
-echo "Please Input IP Client : ";
+printf "\nPlease Input IP Client : ";
 read ipclient;
 ssh-copy-id -i ~/.ssh/id_rsa.pub root@$ipclient 
 
-echo #START INSTALLING ANSIBLE….
-sudo apt-add-repository ppa:ansible/ansible
+sed -i -e "s/x.x.x.x/$ipserver/g" /opt/Report_SSH_Log/Reporter.py
+sed -i -e "s/x.x.x.x/$ipserver/g" /opt/Report_SSH_Log/Client.py
+
+printf "\nSTART INSTALLING MYSQLâ€¦."
+sudo apt install -y python-mysqldb mysql-server python-dev libmysqlclient-dev 
+sudo apt install python-pip -y
+sudo systemctl restart mysql.service
+sudo sed -i -e 's/#bind-address/bind-address/' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo systemctl restart mysql.service
+
+printf "\nSTART INSTALLING ANSIBLEâ€¦."
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update -y
 sudo apt-get install ansible -y
 sudo ansible --version
@@ -38,16 +41,12 @@ $ipclient
 EOF
 sudo ansible -m ping all #testing until showing pong
 
-echo #START INSTALLING MYSQL….
-sudo apt install python-mysqldb mysql-server python-dev libmysqlclient-dev  python-pip –y
-sudo sed -i -e 's/#bind-address/bind-address/' /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo systemctl restart mysql.service
+sudo ansible-playbook /opt/Report_SSH_Log/ansible/ssh-reporting.yml 
 
-cd Report_SSH_Log/
-#Edit
-sed -i –e "s/x.x.x.x/$ipserver/g" Reporter.py
-sed -i –e "s/x.x.x.x/$ipserver/g" Client.py
-cd ansible/
-sudo ansible-playbook ssh-reporting.yml 
+ssh root@$ipclient << EOT
 
+exit
+
+EOT
+
+python /opt/Report_SSH_Log/Reporter.py
